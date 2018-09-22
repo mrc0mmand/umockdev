e65e99f73ce36a78bb244828dd35b8d566032c6e
Frantisek Sumsal <frantisek@sumsal.cz>
2018-09-22 18:46:07 +0200

    tests: support older python versions
    assertRaisesRegexp was renamed to assertRaisesRegex in python 3.2.
    This patch tries to determine the python version and call appropriate
    assertRaiseRegex* method

--- tests/test-umockdev.py
+++ tests/test-umockdev.py
@@ -190,12 +190,17 @@ A: simple_attr=1
             self.assertEqual(f.read(), b'\x41\xFF\x00\x05\xFF\x00')
 
     def test_add_from_string_errors(self):
+        if sys.version_info[0] == 2 or (sys.version_info[0] == 3 and sys.version_info[1] < 2):
+            assertRaisesRegex = self.assertRaisesRegexp
+        else:
+            assertRaisesRegex = self.assertRaisesRegex
+
         # does not start with P:
-        with self.assertRaisesRegex(GLib.GError, 'must start with.*P:') as cm:
+        with assertRaisesRegex(GLib.GError, 'must start with.*P:') as cm:
             self.testbed.add_from_string ('E: SIMPLE_PROP=1\n')
 
         # no value
-        with self.assertRaisesRegex(GLib.GError, 'malformed attribute') as cm:
+        with assertRaisesRegex(GLib.GError, 'malformed attribute') as cm:
             self.testbed.add_from_string ('P: /devices/dev1\nE: SIMPLE_PROP\n')
 
 unittest.main(testRunner=unittest.TextTestRunner(stream=sys.stdout, verbosity=2))
